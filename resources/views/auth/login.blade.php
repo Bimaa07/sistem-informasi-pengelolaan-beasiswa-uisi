7442adbbe14861f3dab0b4544ade8b6e6d1b9389
raflisetiawan <dev.raflisetiawan@gmail.com>
2025-03-28 18:15:04 +0700

    Add Metronic Templates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.auth')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-</head>
-
-@vite(['resources/js/app.js'])
-
-<body>
-    <a href="{{ route('google.login') }}" class="btn btn-danger">
-        <i class="fab fa-google"></i> Login dengan Google
-    </a>
-
-</body>
-
-</html>
+@section('content')
+    <!--begin::Form-->
+    <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" method="POST" action="{{ route('login') }}">
+        @csrf
+        <!--begin::Heading-->
+        <div class="text-center mb-11">
+            <!--begin::Title-->
+            <h1 class="text-gray-900 fw-bolder mb-3">Sign In</h1>
+            <!--end::Title-->
+            <!--begin::Subtitle-->
+            <div class="text-gray-500 fw-semibold fs-6">Sistem Informasi Beasiswa UISI</div>
+            <!--end::Subtitle=-->
+        </div>
+        <!--end::Heading-->
+        <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" data-kt-redirect-url="index.html"
+            action="#">
+            <!--begin::Login options-->
+            <div class="row g-3 mb-9">
+                <!--begin::Col-->
+                <div class="col-md-12">
+                    <!--begin::Google link=-->
+                    <a href="{{ route('google.login') }}"
+                        class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
+                        <img alt="Logo"
+                            src=" {{ Vite::asset('resources/assets/media/svg/brand-logos/google-icon.svg') }} "
+                            class="h-15px me-3" />Sign in
+                        with Google</a>
+                    <!--end::Google link=-->
+                </div>
+                <!--end::Col-->
+                <!--begin::Col-->
+            </div>
+        </form>
+        <!--end::Submit button-->
+    </form>
+    <!--end::Form-->
+@endsection
