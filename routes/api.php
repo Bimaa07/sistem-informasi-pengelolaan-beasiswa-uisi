99a1f47282d1aabfc9b64833da4e8bfca112913b
raflisetiawan <dev.raflisetiawan@gmail.com>
2025-05-13 19:47:16 +0700

    remove unnecessary routes

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::prefix('v1')->group(function () {
-    Route::post('get-mahasiswa', [ApiMahasiswaController::class, 'getMahasiswaByNim'])
-        ->name('api.get-mahasiswa');
-});
